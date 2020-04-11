f919478393c7ec159d107c2c1a82ed6292934c2b
luke williams <jluke94williams@gmail.com>
2020-04-11 12:56:09 -0400

    Added connection for mysql

--- Server.js
+++ Server.js
@@ -1,2 +1,20 @@
 let mySQL = require("mysql")
 let inquirer = require("inquirer")
+
+
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Mustangs!11",
+    database: ""
+});
+  
+// Initiate MySQL Connection.
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
