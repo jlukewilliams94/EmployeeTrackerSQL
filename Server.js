b59552e91e5adf13fb88796922423306b435965d
luke williams <jluke94williams@gmail.com>
2020-04-13 21:22:47 -0400

    Finalized COmmentary

--- Server.js
+++ Server.js
@@ -4,12 +4,12 @@ const cTable = require("console.table")
 
 require('events').EventEmitter.prototype._maxListeners = 100;
 
-
+//Stores connection variables to mySQL (PLEASE INPUT PASSWORD)
 let connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Mustangs!11",
+    password: "",
     database: "employee_management_db"
 });
   
@@ -319,6 +319,7 @@ function viewDeptBudget(){
   })
 }
 
+// UPDATE FUNCTIONS
 //updateInfo to allow the user to update employee table. Department and Role tables do not have this capability at this time. 
 function updateInfo(option) {
   switch(option){
